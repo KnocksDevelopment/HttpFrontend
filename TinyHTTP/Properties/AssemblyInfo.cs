5570b0a3fd9e91b825da852cc81f7812bdf7f812
w0nder <wonder@teamimmersion.net>
2014-03-18 20:44:02 -0700

    Modified Assembly Info

--- TinyHTTP/Properties/AssemblyInfo.cs
+++ TinyHTTP/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TinyHTTP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Small HTTP frontend for .Net Framework.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Immersion")]
 [assembly: AssemblyProduct("TinyHTTP")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Team Immersion 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
