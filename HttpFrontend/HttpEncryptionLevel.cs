3792d7bc0df8983c9e999f63d198245c2f9c0968
w0nder <wonder@teamimmersion.net>
2014-03-19 10:05:00 -0700

    Full project rename, because TinyHTTP is already taken by another project.

---
+++ HttpFrontend/HttpEncryptionLevel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HttpFrontend
+{
+    public class HttpEncryptionLevel
+    {
+        protected readonly string Prefix;
+        private HttpEncryptionLevel(string urlprefix)
+        {
+            if (urlprefix == null) // we should never need this lol
+                throw new ArgumentNullException("urlprefix");
+            Prefix = urlprefix;
+        }
+
+        public string UrlPrefix { get { return Prefix; } }
+
+        public static readonly HttpEncryptionLevel Http = new HttpEncryptionLevel("http://");
+        public static readonly HttpEncryptionLevel Https = new HttpEncryptionLevel("https://");
+    }
+}
---
+++ HttpFrontend/HttpRequestResult.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+
+namespace HttpFrontend
+{
+    public class HttpRequestResult
+    {
+        protected readonly string ResultBody;
+        protected readonly HttpStatusCode ResultCode;
+        protected HttpContentType ResultContentType;
+
+        #region Constructors
+        /// <summary>
+        /// Wrapper for a successfuly HTTP Response.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="?"></param>
+        public HttpRequestResult(string data, HttpStatusCode code)
+        {
+            if (data == null) // shouldn't ever happen
+                throw new ArgumentNullException("data");
+            this.ResultBody = data;
+            this.ResultCode = code;
+        }
+        #endregion
+        #region Accessors
+
+        /// <summary>
+        /// Provides access to the returned Request body.
+        /// </summary>
+        public string Body { get { return ResultBody; } }
+        /// <summary>
+        /// Provides access to the returned Request code.
+        /// </summary>
+        public HttpStatusCode Code { get { return ResultCode; } }
+
+        /// <summary>
+        /// The given content type of the response.
+        /// </summary>
+        public HttpContentType ContentType { get { return ResultContentType; }  set { ResultContentType = value; }}
+
+        #endregion
+    }
+}
